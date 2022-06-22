9c00f5cfc814d31f163a58a9003995553fd1c113
hysts <hysts@users.noreply.huggingface.co>
2022-06-22 19:39:27 +0900

    Copy files from https://github.com/hysts/CogView2_demo
    Needs to be modified to run on Hugging Face Spaces

--- app.py
+++ app.py
@@ -1,32 +1,108 @@
-import os
+#!/usr/bin/env python
+
+from __future__ import annotations
+
+import argparse
+
 import gradio as gr
-os.system("pip install torch==1.10.1+cu111 torchvision==0.11.2+cu111 torchaudio==0.10.1 -f https://download.pytorch.org/whl/torch_stable.html")
-os.system("git clone https://github.com/Sleepychord/Image-Local-Attention")
-os.chdir("Image-Local-Attention")
-os.system("python setup.py install")
-os.chdir("..")
-os.system("git clone https://github.com/NVIDIA/apex")
-os.chdir("apex")
-os.system('pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./')
-os.chdir("..")
-os.system("git clone https://github.com/THUDM/CogView2")
-os.chdir("CogView2")
-os.system("gdown https://drive.google.com/uc?id=1-2nI2TTUOdiQ2WpydGafk_bZIZggQBK4")
-os.system("7za x coglm.zip")
-os.system("gdown https://drive.google.com/uc?id=1ulfXJFstYZUestvWcQIadKkNNDVbpIdM")
 
-def inference(text):
-    with open("input.txt") as f:
-        lines = f.readlines()
-    lines[0] = text
-    with open("input.txt", "w") as f:
-        f.writelines(lines)
-    os.system("python cogview2_text2image.py --mode inference --fp16 --input-source input.txt --output-path samples_sat_v0.2 --batch-size 4 --max-inference-batch-size 8 --only-first-stage")
-    return "/content/CogView2/output.png"
+from model import AppModel
 
-gr.Interface(inference,"text","image",title="CogView 2").launch(debug=True,enable_queue=True)
+DESCRIPTION = '''# CogView2 (text2image)
+
+This is an unofficial demo for <a href="https://github.com/THUDM/CogView2">https://github.com/THUDM/CogView2</a>.
+
+[This Space](https://huggingface.co/spaces/chinhon/translation_eng2ch) is used for translation from English to Chinese.
+'''
 
 
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--only-first-stage', action='store_true')
+    parser.add_argument('--share', action='store_true')
+    return parser.parse_args()
 
 
+def set_example_text(example: list) -> dict:
+    return gr.Textbox.update(value=example[0])
 
+
+def main():
+    args = parse_args()
+    model = AppModel(args.only_first_stage)
+
+    with gr.Blocks(css='style.css') as demo:
+        gr.Markdown(DESCRIPTION)
+
+        with gr.Row():
+            with gr.Column():
+                with gr.Group():
+                    text = gr.Textbox(label='Input Text')
+                    translate = gr.Checkbox(label='Translate to Chinese',
+                                            value=False)
+                    style = gr.Dropdown(choices=[
+                        'mainbody',
+                        'photo',
+                        'flat',
+                        'comics',
+                        'oil',
+                        'sketch',
+                        'isometric',
+                        'chinese',
+                        'watercolor',
+                    ],
+                                        label='Style')
+                    seed = gr.Slider(0,
+                                     100000,
+                                     step=1,
+                                     value=1234,
+                                     label='Seed')
+                    only_first_stage = gr.Checkbox(
+                        label='Only First Stage',
+                        value=args.only_first_stage,
+                        visible=not args.only_first_stage)
+                    num_images = gr.Slider(1,
+                                           16,
+                                           step=1,
+                                           value=8,
+                                           label='Number of Images')
+                    with open('samples.txt') as f:
+                        samples = [[line.strip()] for line in f.readlines()]
+                    examples = gr.Dataset(components=[text], samples=samples)
+                    run_button = gr.Button('Run')
+
+            with gr.Column():
+                with gr.Group():
+                    translated_text = gr.Textbox(label='Translated Text')
+                    with gr.Tabs():
+                        with gr.TabItem('Output (Grid View)'):
+                            result_grid = gr.Image(show_label=False)
+                        with gr.TabItem('Output (Gallery)'):
+                            result_gallery = gr.Gallery(show_label=False)
+
+        run_button.click(fn=model.run_with_translation,
+                         inputs=[
+                             text,
+                             translate,
+                             style,
+                             seed,
+                             only_first_stage,
+                             num_images,
+                         ],
+                         outputs=[
+                             translated_text,
+                             result_grid,
+                             result_gallery,
+                         ])
+        examples.click(fn=set_example_text,
+                       inputs=examples,
+                       outputs=examples.components)
+
+    demo.launch(
+        enable_queue=True,
+        share=args.share,
+    )
+
+
+if __name__ == '__main__':
+    main()
